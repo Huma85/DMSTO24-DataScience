440b32849406ca28f8ec2e0780c74cdcb4106f90
Huma <huma.rashid85@yahoo.com>
2024-12-18 22:37:14 +0100

    Beskriv din ändring här

---
+++ codes.py
@@ -0,0 +1,23 @@
+"""
+
+x = "7"
+y = 22
+print(int(x) +int(22))
+
+I = input("Ange förnamn: ")
+z = input("Ange efternamn: ")
+print(I, z)
+"""
+a = "Banana"
+print(a[0:2])
+
+x = "Huma"
+y = 25
+print(f"Hej mitt namn är {x}, jag är {y} år gammal")
+
+# lowercase och uppercase string metod och capatilize.
+
+b = "FARSTA"
+print(b.lower())
+
+
